608fde65952b98b4e6dbf33b91f5c0a8c667d2d4
xayyeem <kjafri30izback@gmail.com>
2023-10-28 23:39:21 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+// import ReactDOM from 'react-dom/client
+import { render } from 'react-dom'
 import App from './App.jsx'
 
+render( <React.StrictMode>
+  <App />
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+</React.StrictMode>, document.getElementById('root')
 )
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+ 
+// )
